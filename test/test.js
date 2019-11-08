a2e24bdcc49121a80466942b1219549fc157afa4
Jack Cranston <jcranston94@gmail.com>
2019-11-08 16:38:35 +0000

    updated readme with basic information on unit testing

--- test/test.js
+++ test/test.js
@@ -1,9 +1,35 @@
-/* test/sum.js */
-
 const app = require("../app");
 const sum = app.sum;
-const expect = require("chai").expect;
 
+const assert = require("chai").assert;
+const expect = require("chai").expect;
+const should = require("chai").should();
+
+describe("#testing Mocha features", function() {
+  context.skip("should skip this test", function() {
+    // This is skipped so it shouldn't run
+  });
+});
+
+describe("#testing", function() {
+  context("the sum", function() {
+    it("should return 2", function() {
+      expect(1 + 1).to.equal(2);
+    });
+  });
+  context("the number", function() {
+    it("should be a number", function() {
+      assert.isNumber(2);
+    });
+  });
+  context("the array", function() {
+    it("should have a length of 3", function() {
+      [1, 3, 0].should.have.lengthOf(3);
+    });
+  });
+});
+
+/*
 describe("#sum()", function() {
   context("without arguments", function() {
     it("should return 0", function() {
@@ -17,3 +43,4 @@ describe("#sum()", function() {
     });
   });
 });
+*/
